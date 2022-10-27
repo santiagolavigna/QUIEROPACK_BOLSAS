1aec897001fa9b67e7d198e496f0df74bb722e97
Santiago Lavigna Jara <santiagolavigna1@gmail.com>
2022-10-27 15:21:45 -0300

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+/*
+-Set BD
+*/
+  define( 'DB_HOST', 'localhost:3306' );          // Set host
+  define( 'DB_USER', 'root' );             // Set usuario
+  define( 'DB_PASS', '' );             // Set password '' para quieropack
+  define( 'DB_NAME', 'quieropack_bolsas' );        // Set database              
+  
+?>
+
